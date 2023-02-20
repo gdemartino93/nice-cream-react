99d85967a3c8418807bb56123febf31808e95ddb
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-20 23:50:29 +0100

    mapped array

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,26 @@
 import React , {useState} from 'react';
 import data from '../fakeData';
+import Gelato from './Gelato';
 
 const Menu = () => {
     const [product , setProduct] = useState(data);
     // crea un nuovo array di elementi unici dall'array product
     let categories = Array.from(new Set(product.map(el => el.categoria)));
-    console.log(categories);
+    // aggiunge all come primo elemento dell'array
+    categories.unshift('All');
 
   return (
     <div className='menu'>
         <ul>
             {categories.map(el => {
-                return <li> { el.toUpperCase() }</li>
+                return <li key={el.id}> { el.toUpperCase() }</li>
             })}
         </ul>
+        <div className='row row-cols-2 justify-content-around gap-3'>
+          {product.map(el => <Gelato key={el.id} {...el} />)}
+        </div>
     </div>
+    
   )
 }
 
