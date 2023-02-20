3545f81a0b8366fb463884f5b017a4874911669e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-21 00:19:52 +0100

    style component, installing sass

--- src/components/Gelato.js
+++ src/components/Gelato.js
@@ -4,10 +4,16 @@ const Gelato = (el) => {
   return (
     <section className='gelato col-5 d-flex'>
         <div className='gelato-img'>
-            <img src={el.img}></img>
+            <img src={ el.img }></img>
         </div>
         <div className='gelato-body'>
+            <div className='gelato-body-header p-2'>
+                <h5>{ el.nome }</h5>
+                <h6 className='gelato-category'>{ el.categoria }</h6>
+            </div>
+            <div className='gelato-body-body'>
 
+            </div>
         </div>
         
     </section>
